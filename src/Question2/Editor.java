2ce60ad5dc91211d11a9952da333f2b88218936d
datomlins <datomlin@syr.edu>
2025-05-04 12:21:51 -0400

    Made a really ugly, still unusable visual editor

--- src/Question2/Editor.java
+++ src/Question2/Editor.java
@@ -9,33 +9,66 @@ public class Editor
 	private Interpreter interp;
 	private Hardware cpu;
 	
-	JFrame frame;
+	final static boolean shouldFill = true;
+	final static boolean shouldWeightX = true;
+	final static boolean RIGHT_TO_LEFT = false;
 	
 	public Editor()
 	{
 		interp = new Interpreter();
 		cpu = new Hardware();
-		
+				
+		createAndShowGUI();		
+	}
+	
+	private void createAndShowGUI()
+	{
 		// make the frame
-		frame = new JFrame("This is a test because I don't remember how to GUI");
-		frame.setSize(600, 400);
+		JFrame frame = new JFrame("This is a test because I don't remember how to GUI");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		// set up the content pane
+		addComponentsToGUI(frame.getContentPane());
+		
+		// display our panel 
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	private void addComponentsToGUI(Container pane)
+	{
+		if (RIGHT_TO_LEFT)
+		{
+			pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		}
+		
+		pane.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		if (shouldFill)
+		{
+			c.fill = GridBagConstraints.HORIZONTAL; 
+		}
 		
 		// add a space to write code
-		JTextField codeSpace = new JTextField();
+		JTextArea codeSpace = new JTextArea(10, 20);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		pane.add(codeSpace, c);
+		
 		
 		// add a space to show results
 		JLabel label = new JLabel("Current Register State: " + cpu.toString());
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 4;
+		c.gridy = 0;
+		pane.add(label, c);
 		
 		// add a button to execute code
 		JButton executeButton = new JButton("Execute");
-		
-		frame.add(codeSpace);
-		frame.add(label);
-		frame.add(executeButton);
-		
-		// display our panel 
-		frame.setVisible(true);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 2;
+		c.gridy = 5;
+		pane.add(executeButton, c);
 	}
 }
